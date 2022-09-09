0ffbd90f428bd7a7d6533fffc21c4dfad64cbce9
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2022-09-09 20:08:01 -0300

    Crear vehiculos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,33 @@
+import vehiculos.Auto;
+import vehiculos.Moto;
+import vehiculos.Vehiculo;
 
 public class Main {
 	public static void main(String[] args) {
-		
+			Vehiculo v1 = new Vehiculo();
+			
+			Vehiculo a1  = new Auto();
+			
+			Auto a2 = (Auto) a1;
+			
+			Vehiculo m1 = new Moto();
+			
+			Moto m2 = (Moto) m1;
+			
+			a2.acelerar();
+			
+			m2.doblar();
+			
+			a2.setRodado(25);
+			
+			System.out.println(a2.getRodado());
+			
+			Vehiculo[] ve = {v1,a1,m1};
+			
+			for(Vehiculo ves : ve) {
+				System.out.println(ves.toString());
+				
+			}
+			
 	}
 }
