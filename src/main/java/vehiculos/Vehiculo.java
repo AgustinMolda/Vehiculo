147c2d1311eadab13654474317a6414f7deffbb3
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2022-09-09 14:28:02 -0300

    Creacion de clase vehiculo

---
+++ src/main/java/vehiculos/Vehiculo.java
@@ -0,0 +1,20 @@
+package vehiculos;
+
+public class Vehiculo {
+	private String nombre;
+	
+	
+	public void acelerar() {
+		
+	}
+	public void frenar() {
+		
+	}
+	public void doblar() {
+		
+	}
+	public void pasarCambios() {
+		
+		
+	}
+}
