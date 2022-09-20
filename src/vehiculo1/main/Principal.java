b884799f891761147d5efe2d01d93b9a25f190d7
Agustin Moldavsky <agusting.moldavsky@gmail.com>
2022-09-20 00:16:11 -0300

    Instancias de factory

--- src/vehiculo1/main/Principal.java
+++ src/vehiculo1/main/Principal.java
@@ -1,9 +1,32 @@
 package main;
 
+import vehiculos.Auto;
+import vehiculos.FabricaVehiculo;
+import vehiculos.Moto;
+import vehiculos.Vehiculo;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		Vehiculo a = new Auto("Auto");
+		Auto a2 = (Auto) a;
+		a.avanzar();
+		a2.encender();
 		
+		Vehiculo m = new Moto("Moto");
+		
+		Moto m2 = (Moto) m;
+		m.avanzar();
+		m2.hacerWillie();
+		
+		FabricaVehiculo fabrica = new FabricaVehiculo();
+		
+	  	Auto a4 = (Auto) FabricaVehiculo.fabrica("auto");
+		
+	  	a4.encender();
+	  	
+	  	Moto m3 = (Moto) FabricaVehiculo.fabrica("moto");
+	  	m3.hacerWillie();
 	}
 
 }
